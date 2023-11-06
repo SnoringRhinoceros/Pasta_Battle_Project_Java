2f3f9417e9151c4001f60e5c598334e24652ad4e
David <davidshuschool@gmail.com>
2023-11-05 19:02:08 -0600

    Now added more recipes for items to craft

--- src/main/java/com/example/generaltemplate/CraftingRecipes.java
+++ src/main/java/com/example/generaltemplate/CraftingRecipes.java
@@ -3,7 +3,10 @@ package com.example.generaltemplate;
 public enum CraftingRecipes {
     STRAIGHT_SWORD (PossibleActions.BASIC_SWORD, 2, PossibleActions.STRAIGHT_SWORD),
     MEAT_HAMMER (PossibleActions.BASIC_HAMMER, 2, PossibleActions.MEAT_HAMMER),
-    MARINARA_SAUCE (PossibleActions.BASIC_SAUCE, 2, PossibleActions.MARINARA_SAUCE);
+    MARINARA_SAUCE (PossibleActions.BASIC_SAUCE, 2, PossibleActions.MARINARA_SAUCE),
+    MASTER_SWORD (PossibleActions.STRAIGHT_SWORD, 10, PossibleActions.MASTER_SWORD),
+    PRETTY_BIG_HAMMER (PossibleActions.MEAT_HAMMER, 10, PossibleActions.PRETTY_BIG_HAMMER),
+    BOLOGNESE (PossibleActions.MARINARA_SAUCE, 10, PossibleActions.BOLOGNESE);
 
 
 
--- src/main/java/com/example/generaltemplate/PossibleActions.java
+++ src/main/java/com/example/generaltemplate/PossibleActions.java
@@ -10,18 +10,27 @@ public enum PossibleActions {
     STRAIGHT_SWORD(new Action("Straight sword", ActionGroupings.WEAPONS,
             new StatModifier("Straight sword", new Stats(0, 10, 0, 0, 0)),
             "A decent blade for a decent pasta.\n+10 damage", 50)),
+    MASTER_SWORD (new Action("Master sword", ActionGroupings.WEAPONS
+            , new StatModifier("Master Sword", new Stats(0, 15, 0, 5, 0)),
+            "Perfectly balanced as all things should be\n+15 strength and +5 awesomeness", 100)),
     BASIC_HAMMER (new Action("Basic hammer", ActionGroupings.WEAPONS,
             new StatModifier("Basic hammer", new Stats(0, 10, 0, 0, 0)),
             "Not very accurate but pretty strong\n+10 damage", 25)),
     MEAT_HAMMER(new Action("Meat hammer", ActionGroupings.WEAPONS,
             new StatModifier("Normal hammer", new Stats(0, 20, 0, 0, 0)),
             "Hammer but for meat\n+20 damage", 35)),
+    PRETTY_BIG_HAMMER (new Action("Pretty big hammer", ActionGroupings.WEAPONS,
+            new StatModifier("Pretty big hammer", new Stats(0, 50, 0, 0, 0)),
+            "It's big but not very accurate\n+50 damage", 35)),
     BASIC_SAUCE (new Action("Basic sauce", ActionGroupings.SPELLS,
             new StatModifier("Basic Sauce", 5, new Stats(0,20,0,0,0)),
             "The first sauce a rotini ever learns\n+20 to sauce dmg for 5 ticks", 100)),
     MARINARA_SAUCE (new Action("Marinara sauce", ActionGroupings.SPELLS,
             new StatModifier("Marinara sauce", 10, new Stats(0, 20, 0, 0, 0)),
-            "All good rotini start with the fundamentals\n+20 to sauce dmg for 10 ticks", 100));
+            "All good rotini start with the fundamentals\n+20 to sauce dmg for 10 ticks", 100)),
+    BOLOGNESE (new Action("Bolognese", ActionGroupings.SPELLS,
+            new StatModifier("Bolognese", 10, new Stats(0, 10, 0, 2, 0)),
+            "The ultimate sauce\n+20 damage and +2 awesomeness", 100));
 
 
     private Action action;
